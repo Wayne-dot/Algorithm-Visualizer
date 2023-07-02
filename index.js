eb3ab4aca26e67295f8efae670806fd177bc7da3
Wayne-dot <wayneting.wc@gmail.com>
2023-07-01 22:07:25 -0400

    change

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ for (let i = 0; i < row; i++){
     let r = document.createElement("tr");
     for(let j = 0; j < col; j++){
         let c = document.createElement("td");
+
+        if(i == 0 && j != 0){
+            c.textContent = hr[j-1];
+            c.classList.add("horizontal_cell")
+        }
+
+        else if(j == 0 && i != 0){
+            c.textContent = vr[i];
+            c.classList.add("vertical_cell")
+        }
+        else{
+            c.classList.add("normal_cell")
+        }
+
+        c.addEventListener("click", function(){
+            if(this.style.backgroundColor == "black"){
+                this.style.backgroundColor = "white";
+            }
+            else{
+            this.style.backgroundColor = "black";
+            }
+        });
+
         r.appendChild(c);
     }
 
